fb339822471d8a7e182cbe2e25d95425605ff6fe
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-01-19 15:28:11 -0300

    criando componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import car from "./components/Carrinho"
+import prod from "./components/Produto"
 import './App.css';
 
 class App extends React.Component {
 
   state = {
-    
+
   }
 
   render(){
---
+++ src/components/Produto.js
@@ -0,0 +1,10 @@
+import React from "react"
+class Produto extends React.Component{
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
+export default Produto

7ab261c49bdc5377e16f6d2d51336c602487dc2c
GledsonLucas111 <gledsonlucas111@gmail.com>
2022-01-21 16:49:38 -0300

    Alterações produtos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Prods from "./components/Produtos/Produtos.json"
 
 
 
-const AppContainer = styled.div`
+const AppContainer = styled.body`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   justify-items: center;
@@ -17,48 +17,68 @@ const DivProdutos = styled.div`
   grid-template-columns: repeat(3, 1fr);
   justify-items: center;
 `
+const MainProdutos = styled.main`
+  display: grid;
+  justify-items: center;
+
+`
+const MenuInfos = styled.div`
+  display: flex;
+  justify-items: center;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+
 
 
 
 class App extends React.Component {
 
   state = {
-    produtos: Prods
+    produtos: Prods,
+    order: 1
   }
 
-  render(){
+  updateOrder = (ev) => {
+    this.setState({ order: ev.target.value })
+  }
+
+  render() {
     const listaDeProdutos = this.state.produtos.map((itens) => {
-      return(
-          <Produto
-              imgProduto={itens.imagemUrl}
-              nomeProduto={itens.name}
-             
-              valueProduto={itens.value}
-          />
-            
+      return (
+        <Produto
+          imgProduto={itens.imagemUrl}
+          nomeProduto={itens.name}
+          valueProduto={itens.value}
+        />
       )
-  })
-    return(
-     <AppContainer>
-       <p>Quantidade de produtos: {listaDeProdutos.length}</p>
+    })
+    return (
+      <AppContainer>
+        <h1>FILTROS</h1>
+        <MainProdutos>
+          <MenuInfos>
+            <p>Quantidade de produtos: {listaDeProdutos.length}</p>
+            <span>
+              <label>Ordenação: </label>
+              <select
+                name='order'
+                value={this.state.order}
+                onChange={this.updateOrder}>
+                <option value={1}>Crescente</option>
+                <option value={-1}>Decrescente</option>
+              </select>
+            </span>
+          </MenuInfos>
 
+          <DivProdutos>
+            {listaDeProdutos}
+          </DivProdutos>
 
-      <DivProdutos>
-       {listaDeProdutos}
-      </DivProdutos>
-
-      <span>
-        <label>Ordenação: </label>
-        <select
-          // name=''
-          // value={}
-          // onChange={}
-        >
-          <option>Crescente</option>
-          <option>Decrescente</option>
-        </select>
-      </span>
-     </AppContainer>
+        </MainProdutos>
+        <h1>CARRINHO</h1>
+      </AppContainer>
     )
   }
 }
--- src/components/Produtos/Produto.js
+++ src/components/Produtos/Produto.js
@@ -14,21 +14,23 @@ const CardProdutos = styled.div`
     border: 1px solid black;
     width: 300px;
     align-items: center;
-
+    border-radius: 2%;
 `
 
 const ProdutosImg = styled.img`
 width: 100%;
 &:hover{
-    cursor: pointer;
+  
 }
 `
 const CardButton = styled.button`
     margin: 1px;
-
  &:hover{
     cursor: pointer;
 }
+`
+const ElementGreen = styled.h4`
+    color: green;
 
 `
 
@@ -42,7 +44,7 @@ class Produto extends React.Component {
                     <CardProdutos>
                         <ProdutosImg src={this.props.imgProduto} alt={"Imagem do produto"} />
                         <p>{this.props.nomeProduto}</p>
-                        <p>R$ {this.props.valueProduto}</p>
+                        <ElementGreen>R$ {this.props.valueProduto}</ElementGreen>
                         <CardButton>Adicionar ao carrinho</CardButton>
                     </CardProdutos>
             </ProdutosContainer>
